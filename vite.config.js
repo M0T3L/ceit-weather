95db97ab7151164ca15dfec598c9f0f71cbaf4e1
M0T3L <104972867+M0T3L@users.noreply.github.com>
2025-01-05 20:18:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Port 3000 olarak ayarlandı
+    port: 3000,
     proxy: {
       "/assets": {
-        target: "http://localhost:3000", // Proxy için port 3000
+        target: "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/assets/, "/src/assets"),
       },
